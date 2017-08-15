b414bbf115574048d5d020f371a3ef9df6b20c9b
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:19 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,8 @@ class BoostPythonConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
-    url = "https://github.com/boostorg/python"
+    url = "https://github.com/bincrafters/conan-boost-python"
+    source_url = "https://github.com/boostorg/python"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "python"
@@ -32,7 +33,7 @@ class BoostPythonConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def build(self):
         boost_build = self.deps_cpp_info["Boost.Build"]
@@ -57,4 +58,4 @@ class BoostPythonConan(ConanFile):
         self.copy(pattern="*", dst="lib", src=lib_dir)
 
     def package_info(self):
-        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
+        self.cpp_info.libs = self.collect_libs()
