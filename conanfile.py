a44f939949c5749ac2f6adc49087d24b2f6d2391
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:16:38 -0400

    mass update - change short_name to array - also add user_info for generator

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class BoostPythonConan(ConanFile):
     source_url = "https://github.com/boostorg/python"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "python"
+    lib_short_names = ["python"]
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
     requires =  "Boost.Bind/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
@@ -32,8 +32,9 @@ class BoostPythonConan(ConanFile):
                       #bind3 config0 conversion5 core2 detail5 foreach8 function5 iterator5 lexical_cast8 mpl5 numeric~conversion6 preprocessor0 smart_ptr4 static_assert1 tuple4 type_traits3 utility5
 
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.source_url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def build(self):
         boost_build = self.deps_cpp_info["Boost.Build"]
@@ -52,9 +53,12 @@ class BoostPythonConan(ConanFile):
         self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
         
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
+
         self.copy(pattern="*", dst="lib", src="stage/lib")
 
     def package_info(self):
+        self.user_info.lib_short_names = self.lib_short_names
         self.cpp_info.libs = self.collect_libs()
