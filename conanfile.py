08050d7c22233f2b2d6de656d3c390b2314c9d52
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 01:59:32 -0400

    add shared option change source method append BOOST_ALL_NO_LIB

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,11 @@ class BoostPythonConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-python"
-    source_url = "https://github.com/boostorg/python"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["python"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     source_only_deps = ["graph", "multi_index", "parameter","property_map",
         "serialization", "unordered"]
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
@@ -35,26 +36,25 @@ class BoostPythonConan(ConanFile):
                       #bind3 config0 conversion5 core2 detail5 foreach8 function5 iterator5 lexical_cast8 mpl5 numeric~conversion6 preprocessor0 smart_ptr4 static_assert1 tuple4 type_traits3 utility5
 
     def source(self):
-        for lib_short_name in self.lib_short_names + self.source_only_deps:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + version        
+        for lib_short_name in self.lib_short_names:
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def build(self):
-        boost_build = self.deps_cpp_info["Boost.Build"]
-        b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
-        b2_bin_dir_name = boost_build.bindirs[0]
-        b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
-        
         self.run(self.deps_user_info['Boost.Generator'].b2_command
             + ' ' + ' '.join('include=' + dep + '/include' for dep in self.source_only_deps))
                 
+
     def package(self):
+        self.copy(pattern="*", dst="lib", src="stage/lib")
         for lib_short_name in self.lib_short_names:
             include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
-
-        self.copy(pattern="*", dst="lib", src="stage/lib")
+            self.copy(pattern="*", dst="include", src=include_dir)
 
     def package_info(self):
-        self.user_info.lib_short_names = (",").join(self.lib_short_names)
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
         self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
