628d75b005da59bb8919ba9d3abe8c8f98456c50
Rene Rivera <grafikrobot@gmail.com>
2017-09-15 22:19:10 -0500

    Add "python" option and fix building.
    Add "python" option to control which python installation to build
    against. Also fix building, as it broke at some point. We add the
    source_only_deps back in to fetching in the source method.

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,8 @@ class BoostPythonConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["python"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"shared": [True, False], "python": "ANY"}
+    default_options = "shared=False", "python=python"
     source_only_deps = ["graph", "multi_index", "parameter","property_map",
         "serialization", "unordered"]
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
@@ -37,8 +37,8 @@ class BoostPythonConan(ConanFile):
 
     def source(self):
         boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
+        archive_name = "boost-" + self.version
+        for lib_short_name in self.lib_short_names+self.source_only_deps:
             tools.get("{0}/{1}/archive/{2}.tar.gz"
                 .format(boostorg_github, lib_short_name, archive_name))
             os.rename(lib_short_name + "-" + archive_name, lib_short_name)
@@ -46,7 +46,6 @@ class BoostPythonConan(ConanFile):
     def build(self):
         self.run(self.deps_user_info['Boost.Generator'].b2_command
             + ' ' + ' '.join('include=' + dep + '/include' for dep in self.source_only_deps))
-                
 
     def package(self):
         self.copy(pattern="*", dst="lib", src="stage/lib")
@@ -58,3 +57,15 @@ class BoostPythonConan(ConanFile):
         self.user_info.lib_short_names = ",".join(self.lib_short_names)
         self.cpp_info.libs = self.collect_libs()
         self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
+
+    def package_id(self):
+        class get_pyver():
+            def __init__(self):
+                self.value = ""
+            def write(self,m):
+                self.value = self.value+m.strip()
+        pyver = get_pyver()
+        self.run(
+            '''{0} -c "from sys import *; print('%d.%d' % (version_info[0],version_info[1]))"'''.format(self.info.options.python),
+            output=pyver)
+        self.info.options.python = "python-"+pyver.value
