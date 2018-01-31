0de8654d449d063f3039d8e1c3fcf1190d5ab09f
Jerry W <jerrywiltse@gmail.com>
2018-01-31 15:11:47 -0500

    try pass python path appveyor

--- build.py
+++ build.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+from bincrafters import build_shared
 from bincrafters import build_template_boost_default
 
-
+python_path = os.getenv("CONAN_BOOST_PYTHON_PATH", "")
+args = "-o boost_python:python=" + python_path
+    
 if __name__ == "__main__":
-    builder = build_template_boost_default.get_builder()
+
+    builder = build_template_boost_default.get_builder(args=args)
+
     builder.run()
