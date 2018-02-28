682263add2a3b56d32b9a33afc7007e0632af3af
SSE4 <tomskside@gmail.com>
2018-02-28 23:02:48 +0700

    - python 3 uses different prefix (PyInit_)

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -13,10 +13,15 @@ BOOST_PYTHON_MODULE(embedded_hello)
 	python::def("greet",greet);
 }
 
+
 int main()
 {
 	Py_Initialize();
-	if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
+#  if PY_VERSION_HEX >= 0x03000000
+    if (PyImport_AppendInittab("embedded_hello", PyInit_embedded_hello) == -1)
+#else
+    if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
+#endif
 		throw std::runtime_error("Failed");
 	python::object main = python::import("__main__");
 	python::object global(main.attr("__dict__"));
