c218220f3db1464cc98a3ef0fa1f88aea95a9487
SSE4 <tomskside@gmail.com>
2018-03-01 00:31:32 +0700

    - PyImport_AppendInittab must be called prior to the Py_Initialize

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -16,13 +16,14 @@ BOOST_PYTHON_MODULE(embedded_hello)
 
 int main()
 {
-	Py_Initialize();
 #  if PY_VERSION_HEX >= 0x03000000
     if (PyImport_AppendInittab("embedded_hello", PyInit_embedded_hello) == -1)
 #else
     if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
 #endif
-		throw std::runtime_error("Failed");
+        throw std::runtime_error("Failed");
+	Py_Initialize();
+
 	python::object main = python::import("__main__");
 	python::object global(main.attr("__dict__"));
 	python::object result = python::exec(
