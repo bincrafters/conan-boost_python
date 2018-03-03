94d60c67fc383ae1becb44e986d7733c615bfd01
Rene Rivera <grafikrobot@gmail.com>
2018-03-03 16:02:48 -0600

    Use python_dev_config package for configuration of Python for building. [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, tools
-from io import StringIO
-import os
 
 
 class BoostPythonConan(ConanFile):
@@ -15,13 +13,14 @@ class BoostPythonConan(ConanFile):
     lib_short_names = ["python"]
     is_header_only = False
 
-    options = {"shared": [True, False], "python": "ANY"}
-    default_options = "shared=False", "python=python"
+    options = {"shared": [True, False], "python_version": "ANY"}
+    default_options = "shared=False"
 
     source_only_deps = ["graph", "multi_index", "parameter",
     "property_map", "serialization", "unordered"]
 
     requires = (
+        "python_dev_config/0.1@bincrafters/testing",
         "boost_package_tools/1.66.0@bincrafters/testing",
         "boost_bind/1.66.0@bincrafters/testing",
         "boost_config/1.66.0@bincrafters/testing",
@@ -43,98 +42,19 @@ class BoostPythonConan(ConanFile):
     )
 
     def package_info_additional(self):
-        self.cpp_info.includedirs.append(self.python_include)
-        self.cpp_info.libdirs.append(os.path.dirname(self.python_lib))
-        self.cpp_info.libs.append(os.path.basename(self.python_lib))
         if self.options.shared:
             self.cpp_info.defines.append('BOOST_PYTHON_DYNAMIC_LIB')
         else:
             self.cpp_info.defines.append('BOOST_PYTHON_STATIC_LIB')
-        self.cpp_info.bindirs.append(self.python_bin)
 
     def package_id_additional(self):
-        self.info.options.python = "python-" + self.python_version
-
         boost_deps_only = [dep_name for dep_name in self.info.requires.pkg_names if dep_name.startswith("boost_")]
         for dep_name in boost_deps_only:
             self.info.requires[dep_name].full_version_mode()
-
-    def system_requirements(self):
-        if self.settings.os == "Linux" and tools.os_info.is_linux:
-            if tools.os_info.with_apt:
-                if self.settings.arch == 'x86':
-                    arch_suffix = ':i386'
-                elif self.settings.arch == 'x86_64':
-                    arch_suffix = ':amd64'
-                package_tool = tools.SystemPackageTool()
-                package_tool.install("python-dev%s" % arch_suffix)
-            elif tools.os_info.with_yum:
-                if self.settings.arch == 'x86':
-                    arch_suffix = '.i686'
-                elif self.settings.arch == 'x86_64':
-                    arch_suffix = '.x86_64'
-                package_tool = tools.SystemPackageTool()
-                package_tool.install("python-devel%s" % arch_suffix)
-
-    @property
-    def python_exec(self):
-        try:
-            pyexec = str(self.options.python)
-            output = StringIO()
-            self.run('{0} -c "import sys; print(sys.executable)"'.format(pyexec), output=output)
-            return output.getvalue().strip().replace("\\", "/")
-        except:
-            return ""
-
-    @property
-    def python_include(self):
-        pyinclude = self.get_python_path("include")
-        if not os.path.exists(os.path.join(pyinclude, 'pyconfig.h')):
-            pyinclude = self.get_python_var('INCLUDEPY')
-            if not os.path.exists(os.path.join(pyinclude, 'pyconfig.h')):
-                return ""
-            else:
-                return pyinclude.replace('\\', '/')
-        else:
-            return pyinclude.replace('\\', '/')
-
-    @property
-    def python_version(self):
-        cmd = "from sys import *; print('%d.%d' % (version_info[0],version_info[1]))"
-        return self.run_python_command(cmd)
-
-    @property
-    def python_version_nodot(self):
-        cmd = "from sys import *; print('%d%d' % (version_info[0],version_info[1]))"
-        return self.run_python_command(cmd)
-
-    @property
-    def python_lib(self):
-        py_stdlib = self.get_python_path("stdlib").replace('\\', '/')
-        if self.settings.os == "Windows":
-            py_stdlib = os.path.join(os.path.dirname(py_stdlib), "libs", "python"+self.python_version_nodot+".lib")
-        return py_stdlib
-
-    @property
-    def python_bin(self):
-        return self.get_python_var('BINDIR')
-
-    def get_python_var(self, var_name):
-        cmd = "import sysconfig; print(sysconfig.get_config_var('{0}'))".format(var_name)
-        return self.run_python_command(cmd)
-
-    def get_python_path(self, dir_name):
-        cmd = "import sysconfig; print(sysconfig.get_path('{0}'))".format(dir_name)
-        return self.run_python_command(cmd)
-
-    def run_python_command(self, cmd):
-        pyexec = self.python_exec
-        if pyexec:
-            output = StringIO()
-            self.run('{0} -c "{1}"'.format(pyexec, cmd), output=output)
-            return output.getvalue().strip()
-        else:
-            return ""
+    
+    def source_additional(self):
+        if self.options.python_version != self.deps_user_info['python_dev_config'].python_version:
+            raise Exception("Python version does not match with configured python dev, expected %s but got %s." % (self.options.python_version, self.deps_user_info['python_dev_config'].python_version))
 
     # BEGIN
 
@@ -171,7 +91,5 @@ class BoostPythonConan(ConanFile):
             import boost_package_tools  # pylint: disable=F0401
             boost_package_tools.package_info(self)
         getattr(self, "package_info_additional", lambda:None)()
-
-
 
     # END
