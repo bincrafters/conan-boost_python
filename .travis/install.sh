52088541e848a126aa7082bf3d579dad85557b43
Rene Rivera <grafikrobot@gmail.com>
2018-03-10 23:56:02 -0600

    Adjust options and paths for use of python_dev_config.

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,8 +13,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install $PYENV_VERSION
+    pyenv virtualenv $PYENV_VERSION conan
     pyenv rehash
     pyenv activate conan
 fi
--- build.py
+++ build.py
@@ -5,9 +5,14 @@ import os
 from bincrafters import build_shared
 from bincrafters import build_template_boost_default
 
-python_path = os.getenv("CONAN_BOOST_PYTHON_PATH", "python")
-args = "-o boost_python:python=" + python_path
-    
+args = ""
+python_version = os.getenv("CONAN_BOOST_PYTHON_VERSION", None)
+if python_version:
+    args += "-o boost_python:python_version=" + python_version
+python_path = os.getenv("CONAN_PYTHON_PATH", "python")
+if python_path:
+    args += "-o python_dev_config:python=" + python_path
+
 if __name__ == "__main__":
 
     builder = build_template_boost_default.get_builder(args=args)
