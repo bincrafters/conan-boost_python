8bc44e87cc2b6d2d28079a654fb7a94bc901d859
Jerry Wiltse <jwiltse@carsim.com>
2018-08-15 18:14:49 -0400

    add missing parens for build.py

--- build.py
+++ build.py
@@ -5,16 +5,18 @@ import os
 from bincrafters import build_shared
 from bincrafters import build_template_boost_default
 
-args = []
-python_version = os.getenv("CONAN_BOOST_PYTHON_VERSION", None)
-if python_version:
-    args.extend["-o","boost_python:python_version=" + python_version]
-python_path = os.getenv("CONAN_BOOST_PYTHON_PATH", "python")
-if python_path:
-    args.extend["-o","python_dev_config:python=" + python_path]
     
 if __name__ == "__main__":
 
+    args = []
+    python_version = os.getenv("CONAN_BOOST_PYTHON_VERSION", None)
+    python_path = os.getenv("CONAN_BOOST_PYTHON_PATH", "python")
+    
+    if python_version:
+        args.extend(["-o","boost_python:python_version=" + python_version])
+    if python_path:
+        args.extend(["-o","python_dev_config:python=" + python_path])
+    
     builder = build_template_boost_default.get_builder(args=args)
 
     builder.run()
