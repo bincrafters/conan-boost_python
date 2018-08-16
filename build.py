8df547a3c173896088bb2c3666ef5b92f67c49c1
Jerry W <jerrywiltse@gmail.com>
2018-08-16 03:43:40 -0400

    try move build_requires to build.py to satisfy travis and appveyor in same place

--- build.py
+++ build.py
@@ -13,12 +13,16 @@ if __name__ == "__main__":
         "python_dev_config:python" : os.getenv("CONAN_PYTHON_PATH", "python"),
     }
     
+    updated_build_requires = {
+        "*": "python_dev_config/0.4@bincrafters/stable"
+    }
+    
     builder = build_template_boost_default.get_builder()
 
     builds_with_updated_options = []
 
     for settings, options, env_vars, build_requires, reference in builder.items:
-         builds_with_updated_options.append([settings, updated_options, env_vars, build_requires])
+         builds_with_updated_options.append([settings, updated_options, env_vars, updated_build_requires])
     
     builder.builds = builds_with_updated_options    
     
