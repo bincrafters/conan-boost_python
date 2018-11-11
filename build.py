c2b6c8df061ae8e6cd7c82b4e8d2f588fc1aa8cf
Jerry W <jerrywiltse@gmail.com>
2018-11-11 09:38:15 -0500

    removed un-necessary build.py option injection [ci skip]

--- build.py
+++ build.py
@@ -8,11 +8,6 @@ from bincrafters import build_template_boost_default
     
 if __name__ == "__main__":
 
-    updated_options = {
-        "boost_python:python_version" : os.getenv("CONAN_BOOST_PYTHON_VERSION", None),
-        "python_dev_config:python" : os.getenv("CONAN_PYTHON_PATH", "python"),
-    }
-    
     builder = build_template_boost_default.get_builder()
     
     builder.run()
